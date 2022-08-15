6629f142b4be1651e7ca1f449214c5172377c4dc
Dengke Tang <dengket@amazon.com>
2022-08-15 15:26:37 -0700

    use the load target default function

--- cmake/aws-c-sdkutils-config.cmake
+++ cmake/aws-c-sdkutils-config.cmake
@@ -1,9 +1,6 @@
 include(CMakeFindDependencyMacro)
+include(AwsCrtLoadTarget)
 
 find_dependency(aws-c-common)
 
-if (BUILD_SHARED_LIBS)
-    include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
-else()
-    include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
-endif()
+aws_load_target_default(@PROJECT_NAME@)
