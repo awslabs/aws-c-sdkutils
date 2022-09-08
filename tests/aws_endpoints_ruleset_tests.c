d751ac24f083f247b077e123ef20f54807de1e6f
Dmitriy Musatkin <musatkd@amazon.com>
2022-09-08 04:05:22 -0700

    win fix

--- tests/aws_endpoints_ruleset_tests.c
+++ tests/aws_endpoints_ruleset_tests.c
@@ -10,16 +10,18 @@
 #include <aws/testing/aws_test_harness.h>
 #include <time.h>
 
-static int read_file_contents(struct aws_byte_buf *out_buf, struct aws_allocator *alloc, const char *filename) {
+static int read_file_contents(struct aws_byte_buf *out_buf, struct aws_allocator *alloc, const struct aws_string *filename) {
     AWS_ZERO_STRUCT(*out_buf);
-    FILE *fp = fopen(filename, "r");
+    struct aws_string *mode = aws_string_new_from_c_str(alloc, "r");
+    FILE *fp = aws_fopen_safe(filename, mode);
+    aws_string_destroy(mode);
     ASSERT_NOT_NULL(fp);
 
     int64_t file_size = 0;
     ASSERT_INT_EQUALS(aws_file_get_length(fp, &file_size), AWS_OP_SUCCESS);
 
-    ASSERT_INT_EQUALS(aws_byte_buf_init(out_buf, alloc, file_size), AWS_OP_SUCCESS);
-    size_t read = fread(out_buf->buffer, 1, file_size, fp);
+    ASSERT_INT_EQUALS(aws_byte_buf_init(out_buf, alloc, (size_t)file_size), AWS_OP_SUCCESS);
+    size_t read = fread(out_buf->buffer, 1, (size_t)file_size, fp);
     fclose(fp);
 
     ASSERT_INT_EQUALS(file_size, read);
@@ -35,7 +37,10 @@ static int s_test_parse_ruleset_from_string(struct aws_allocator *allocator, voi
 
     struct aws_byte_buf buf;
     aws_sdkutils_library_init(allocator);
-    ASSERT_INT_EQUALS(read_file_contents(&buf, allocator, "sample_ruleset.json"), AWS_OP_SUCCESS);
+
+    struct aws_string *filename = aws_string_new_from_c_str(allocator, "sample_ruleset.json");
+    
+    ASSERT_INT_EQUALS(read_file_contents(&buf, allocator, filename), AWS_OP_SUCCESS);
     struct aws_byte_cursor ruleset_json = aws_byte_cursor_from_buf(&buf);
 
     clock_t begin = clock();
@@ -46,6 +51,7 @@ static int s_test_parse_ruleset_from_string(struct aws_allocator *allocator, voi
 
     ASSERT_NOT_NULL(ruleset);
 
+    aws_string_destroy(filename);
     aws_endpoints_ruleset_release(ruleset);
     aws_byte_buf_clean_up(&buf);
     aws_sdkutils_library_clean_up();
