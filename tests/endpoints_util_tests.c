c3247c4d0faa3a8b57d6ddff0fe3fca5889b16d6
Dmitriy Musatkin <musatkd@amazon.com>
2022-10-26 18:08:31 -0700

    fix bug in resolving templates if strings contain escaped quotes

--- tests/endpoints_util_tests.c
+++ tests/endpoints_util_tests.c
@@ -136,3 +136,36 @@ static int s_test_uri_normalize_path(struct aws_allocator *allocator, void *ctx)
 
     return AWS_OP_SUCCESS;
 }
+
+int s_resolve_cb(struct aws_byte_cursor template, void *user_data, struct aws_owning_cursor *out_resolved) {
+    *out_resolved = aws_endpoints_non_owning_cursor_create(aws_byte_cursor_from_c_str("test"));
+    return AWS_OP_SUCCESS;
+}
+
+AWS_TEST_CASE(endpoints_byte_buf_init_from_resolved_templated_string, s_test_byte_buf_init_from_resolved_templated_string)
+static int s_test_byte_buf_init_from_resolved_templated_string(struct aws_allocator *allocator, void *ctx) {
+
+    struct aws_byte_buf buf;
+
+    ASSERT_SUCCESS(aws_byte_buf_init_from_resolved_templated_string(allocator, &buf,
+        aws_byte_cursor_from_c_str("{e} a {b}{c} a {d}"), s_resolve_cb, NULL,false));
+    ASSERT_CURSOR_VALUE_CSTRING_EQUALS(aws_byte_cursor_from_buf(&buf), "test a testtest a test");
+    aws_byte_buf_clean_up(&buf);
+
+    ASSERT_SUCCESS(aws_byte_buf_init_from_resolved_templated_string(allocator, &buf,
+        aws_byte_cursor_from_c_str("{ \"a\": \"{b} {d} \", \"c\": \" {e} \"}"), s_resolve_cb, NULL,true));
+    ASSERT_CURSOR_VALUE_CSTRING_EQUALS(aws_byte_cursor_from_buf(&buf), "{ \"a\": \"test test \", \"c\": \" test \"}");
+    aws_byte_buf_clean_up(&buf);
+
+    ASSERT_SUCCESS(aws_byte_buf_init_from_resolved_templated_string(allocator, &buf,
+        aws_byte_cursor_from_c_str("a \" {b} \" a"), s_resolve_cb, NULL,false));
+    ASSERT_CURSOR_VALUE_CSTRING_EQUALS(aws_byte_cursor_from_buf(&buf), "a \" test \" a");
+    aws_byte_buf_clean_up(&buf);
+
+    ASSERT_SUCCESS(aws_byte_buf_init_from_resolved_templated_string(allocator, &buf,
+        aws_byte_cursor_from_c_str("{ \"a\": \"a \\\" {b} \\\" a\" }"), s_resolve_cb, NULL,true));
+    ASSERT_CURSOR_VALUE_CSTRING_EQUALS(aws_byte_cursor_from_buf(&buf), "{ \"a\": \"a \\\" test \\\" a\" }");
+    aws_byte_buf_clean_up(&buf);
+
+    return AWS_OP_SUCCESS;
+}
