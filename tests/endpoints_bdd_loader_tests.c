1e11c50a1b3153335ca9f5b525f4f73647176d08
Krish <krishpy@amazon.com>
2026-03-04 15:24:05 -0800

    testing

--- tests/endpoints_bdd_loader_tests.c
+++ tests/endpoints_bdd_loader_tests.c
@@ -46,3 +46,37 @@ static int s_test_bdd_loader_basic(struct aws_allocator *allocator, void *ctx) {
 
     return AWS_OP_SUCCESS;
 }
+
+AWS_TEST_CASE(endpoints_bdd_compiler_loader_roundtrip, s_test_bdd_compiler_loader_roundtrip)
+static int s_test_bdd_compiler_loader_roundtrip(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
+
+    aws_sdkutils_library_init(allocator);
+
+    struct aws_byte_buf bytecode;
+    ASSERT_SUCCESS(aws_byte_buf_init_from_file(&bytecode, allocator, "simple_bdd_test.bin"));
+
+    struct aws_byte_buf partitions_buf;
+    ASSERT_SUCCESS(aws_byte_buf_init_from_file(&partitions_buf, allocator, "sample_partitions.json"));
+    struct aws_byte_cursor partitions_json = aws_byte_cursor_from_buf(&partitions_buf);
+
+    struct aws_partitions_config *partitions = aws_partitions_config_new_from_string(allocator, partitions_json);
+    ASSERT_NOT_NULL(partitions);
+
+    struct aws_endpoints_bdd_engine *engine = aws_endpoints_bdd_engine_new_from_bytecode(
+        allocator, aws_byte_cursor_from_buf(&bytecode), partitions);
+    ASSERT_NOT_NULL(engine);
+
+    ASSERT_TRUE(aws_byte_cursor_eq_c_str(&engine->version, "1.1"));
+    ASSERT_UINT_EQUALS(2, aws_hash_table_get_entry_count(&engine->parameters));
+    ASSERT_UINT_EQUALS(2, aws_array_list_length(&engine->conditions));
+    ASSERT_UINT_EQUALS(3, aws_array_list_length(&engine->results));
+    ASSERT_UINT_EQUALS(3, engine->node_count);
+
+    aws_endpoints_bdd_engine_release(engine);
+    aws_partitions_config_release(partitions);
+    aws_byte_buf_clean_up(&partitions_buf);
+    aws_byte_buf_clean_up(&bytecode);
+
+    return AWS_OP_SUCCESS;
+}
